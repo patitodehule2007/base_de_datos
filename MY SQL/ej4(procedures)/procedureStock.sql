f00976e0922cd7d1939684bf31e718b210e119e5
= <=>
2026-05-07 11:33:29 -0300

    feat: ej 2

--- MY SQL/ej4(procedures)/procedureStock.sql
+++ MY SQL/ej4(procedures)/procedureStock.sql
@@ -26,10 +26,44 @@ BEGIN
 			LEAVE bucle;
 		END IF;
 		UPDATE producto 
-		SET cantidad = cantidad + v_cantProd
+		SET stock = stock + v_cantProd
 		WHERE codProducto = v_codProd;
 	END LOOP;
 	CLOSE product;
 END ;
+
+DELIMITER ;
+
+-- 2
+DELIMITER //
+
+CREATE PROCEDURE update_prices()
+BEGIN
+    DECLARE  cursor_prices CURSOR FOR (
+    SELECT COUNT(*),codProducto FROM producto
+    Join stocks.pedido_producto pp on producto.codProducto = pp.Producto_codProducto
+    GROUP BY codProducto
+    ) ;
+    DECLARE sigue boolean default TRUE;
+    DECLARE CONTINUE handler for not found set sigue = false;
+
+    DECLARE v_num_Prod int;
+    DECLARE v_cod_Prod int;
+
+
+    OPEN cursor_prices;
+    bucle: loop
+        fetch cursor_prices into v_num_Prod,v_cod_Prod;
+        if(NOT sigue) THEN
+            leave bucle;
+        end if;
+        if(v_num_Prod < 100) THEN
+            UPDATE producto
+                set precio = precio*0.9
+                WHERE producto.codProducto = v_cod_Prod;
+        end if;
+    end loop;
+    CLOSE cursor_prices;
+end;
 
 DELIMITER ;
